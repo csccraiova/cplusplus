4809e5e9d2e16177d360b84914ce926806c0905b
csccraiova <114810672+csccraiova@users.noreply.github.com>
2022-10-01 16:03:48 +0300

    Add files via upload
    Prima lectie de C++

---
+++ C++ L1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+// int g;
+
+int main()
+{
+    int i, a, c;
+    int x,X;
+    x=4;
+    X=5;
+    
+    //cout<<"valoarea lui x="<<x;
+    //cout<<endl<<"X="<<X;
+    
+    unsigned int e = 4; // numar natural
+    
+    double d = 4.5;
+    int dd=1;
+    //cout<<d+dd;
+    
+    //char f = 'e';
+    //cout << f;
+    
+    bool h=true;
+    bool r=false;
+    //cout<<h<<" "<<r;
+    
+   // cout<<"marimea int in memorie este de:"<<sizeof(int)<<" bytes"<<endl;
+   // cout<<"marimea double in memorie este de:"<<sizeof(double)<<" bytes"<<endl;
+    //cout<<"marimea float in memorie este de:"<<sizeof(float)<<" bytes"<<endl;
+   // cout<<"marimea long in memorie este de:"<<sizeof(long)<<" bytes"<<endl;
+   // cout<<"marimea bool in memorie este de:"<<sizeof(bool)<<" bytes\n";
+    //cout<<"marimea unsigned int in memorie este de:"<<sizeof(unsigned int)<<" bytes";
+    
+    //cout<<"salut"<<endl<<"mere\npere";
+    
+    char ccc = 'A';
+    int nr=(int)ccc+32;
+    cout<<(char)nr;
+    
+//    cout<<"introduceti un numar:";
+    
+//    cin>>i;
+    
+//    cout<<i;
+    
+/*    for(i=1;i<5;i++)
+    {
+        if(i==2)
+        {
+            i++;
+        }
+        
+    }
+*/
+    return 0;
+}
+/*
+int cauta_nume()
+{
+    cout<<g;
+}
+*/
